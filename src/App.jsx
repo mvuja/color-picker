21fd4b70ac3e3a9e89c76e4206a48740d881629a
unknown <markowujanovic@gmail.com>
2022-07-10 23:50:48 +0200

    replace hex with rgba

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
-import { HexColorPicker } from "react-colorful";
+import { RgbaColorPicker } from "react-colorful";
 import './_color-picker.scss'
 
 const App = () => {
-  const [color, setColor] = useState("#b32aa9");
+  const [color, setColor] = useState({ r: 200, g: 150, b: 35, a: 0.5 })
 
   return (
     <div className="App">
-      <HexColorPicker color={color} onChange={setColor} />
+      <RgbaColorPicker color={color} onChange={setColor} />
 
-      <div className="value" style={{ borderLeftColor: color }}>
-        Current color is {color}
-      </div>
+      {/* <div className="value">{JSON.stringify(color)}</div> */}
 
       <div className="buttons">
-        <button onClick={() => setColor("#c6ad23")}>Choose gold</button>
-        <button onClick={() => setColor("#556b2f")}>Choose green</button>
-        <button onClick={() => setColor("#207bd7")}>Choose blue</button>
+        <button onClick={() => setColor({ r: 75, g: 75, b: 150, a: 1 })}>
+          Choose blue
+        </button>
+        <button onClick={() => setColor({ r: 50, g: 150, b: 50, a: 1 })}>
+          Choose green
+        </button>
       </div>
     </div>
   );
